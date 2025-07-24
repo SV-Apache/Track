96ef8d1a0e9ec0735963838f7be2fc860a60a286
SV-Apache <90154737+SV-Apache@users.noreply.github.com>
2025-07-24 11:10:28 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'budget-app-cache-v1';
+const urlsToCache = [
+  '/index.html',
+  '/budget.css',
+  '/budget.js',
+  '/manifest.json',
+  '/budget.png',
+  '/money-growth.png'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(urlsToCache);
+    })
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
